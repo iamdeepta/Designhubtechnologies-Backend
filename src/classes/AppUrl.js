fab9380177fc0a2d1b3d11a23645bba2100c3c4b
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-09-14 23:44:07 +0600

    updated

--- src/classes/AppUrl.js
+++ src/classes/AppUrl.js
@@ -1,3 +1,6 @@
 export default class AppUrl {
   static base_url = "http://localhost:8000/api/";
+  static image_url = "http://localhost:8000/";
+  //   static base_url =
+  //   "https://admin.designhubtechnologies.com/backend/public/api/";
 }
--- src/components/HomeSection1.js
+++ src/components/HomeSection1.js
@@ -4,8 +4,141 @@ import "./css/homeSection1.css";
 import { faEdit, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
+import AppUrl from "../classes/AppUrl";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const HomeSection1 = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+  const [image3, setImage3] = useState("");
+  const [image4, setImage4] = useState("");
+
+  const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    axios
+      .get(AppUrl.base_url + "homesection1Get")
+      .then(function (response) {
+        if (response) {
+          setData(response.data);
+          console.log(response.data);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  //update states
+  const [title_up, setTitleUp] = useState(data.homesection1_title);
+  const [description_up, setDescriptionUp] = useState(
+    data.homesection1_description
+  );
+  const [category_up, setCategoryUp] = useState(data.homesection1_category);
+
+  // async function getData() {
+  //   let result = await fetch(AppUrl.base_url + "homesection1Get");
+
+  //   result = await result.json();
+  //   setData(result);
+  //   console.log(data);
+  // }
+
+  //add data
+  async function addData() {
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("category", category);
+    formData.append("image1", image1);
+    formData.append("image2", image2);
+    formData.append("image3", image3);
+    formData.append("image4", image4);
+
+    let result = await fetch(AppUrl.base_url + "homesection1Add", {
+      method: "POST",
+      body: formData,
+    });
+
+    result = await result.json();
+
+    if (result.success) {
+      toast.success(result.success);
+      setTitle("");
+      setDescription("");
+      setCategory("");
+      setImage1("");
+      setImage2("");
+      setImage3("");
+      setImage4("");
+    } else {
+      toast.error(result.error);
+    }
+
+    getData();
+  }
+
+  //delete data
+  async function deleteData(id) {
+    let result = await fetch(AppUrl.base_url + "homesection1Delete/" + id, {
+      method: "POST",
+    });
+
+    result = await result.json();
+
+    if (result.success) {
+      toast.success(result.success);
+    } else {
+      toast.error(result.error);
+    }
+
+    getData();
+    closeDeleteModal(id);
+  }
+
+  // let title1;
+  // data.map((item) => (title1 = item.homesection1_title));
+
+  //console.log(title_up);
+
+  //update data
+  async function updateData(id) {
+    const formData = new FormData();
+    formData.append("title_up", title_up);
+    formData.append("description_up", description_up);
+    formData.append("category_up", category_up);
+
+    let result = await fetch(AppUrl.base_url + "homesection1Update/" + id, {
+      method: "POST",
+      body: formData,
+    });
+
+    result = await result.json();
+
+    if (result.success) {
+      toast.success(result.success);
+
+      getData();
+      closeUpdateModal(id);
+    } else {
+      toast.error(result.error);
+    }
+  }
+
   function openModal() {
     let element = document.getElementById("home_section_modal");
     ReactDOM.findDOMNode(element).classList.add("active_home_section_modal");
@@ -18,7 +151,7 @@ const HomeSection1 = () => {
     ReactDOM.findDOMNode(element1).classList.remove("inactive_modal_blur_bg");
   }
 
-  function closeModal() {
+  function closeModal(id) {
     let element = document.getElementById("home_section_modal");
     ReactDOM.findDOMNode(element).classList.add("inactive_home_section_modal");
     ReactDOM.findDOMNode(element).classList.remove("active_home_section_modal");
@@ -27,12 +160,12 @@ const HomeSection1 = () => {
     ReactDOM.findDOMNode(element1).classList.add("inactive_modal_blur_bg");
     ReactDOM.findDOMNode(element1).classList.remove("active_modal_blur_bg");
 
-    closeUpdateModal();
-    closeDeleteModal();
+    closeUpdateModal(id);
+    closeDeleteModal(id);
   }
 
-  function openUpdateModal() {
-    let element = document.getElementById("home_section_modal_update");
+  function openUpdateModal(id) {
+    let element = document.getElementById("home_section_modal_update" + id);
     ReactDOM.findDOMNode(element).classList.add(
       "active_home_section_modal_update"
     );
@@ -45,8 +178,8 @@ const HomeSection1 = () => {
     ReactDOM.findDOMNode(element1).classList.remove("inactive_modal_blur_bg");
   }
 
-  function closeUpdateModal() {
-    let element = document.getElementById("home_section_modal_update");
+  function closeUpdateModal(id) {
+    let element = document.getElementById("home_section_modal_update" + id);
     ReactDOM.findDOMNode(element).classList.add(
       "inactive_home_section_modal_update"
     );
@@ -59,8 +192,8 @@ const HomeSection1 = () => {
     ReactDOM.findDOMNode(element1).classList.remove("active_modal_blur_bg");
   }
 
-  function openDeleteModal() {
-    let element = document.getElementById("home_section_modal_delete");
+  function openDeleteModal(id) {
+    let element = document.getElementById("home_section_modal_delete" + id);
     ReactDOM.findDOMNode(element).classList.add(
       "active_home_section_modal_delete"
     );
@@ -73,8 +206,8 @@ const HomeSection1 = () => {
     ReactDOM.findDOMNode(element1).classList.remove("inactive_modal_blur_bg");
   }
 
-  function closeDeleteModal() {
-    let element = document.getElementById("home_section_modal_delete");
+  function closeDeleteModal(id) {
+    let element = document.getElementById("home_section_modal_delete" + id);
     ReactDOM.findDOMNode(element).classList.add(
       "inactive_home_section_modal_delete"
     );
@@ -88,6 +221,7 @@ const HomeSection1 = () => {
   }
   return (
     <>
+      <ToastContainer />
       <div className="container home_section_div">
         <div className="home_sections_title">
           <h4>Section 1</h4>
@@ -98,35 +232,68 @@ const HomeSection1 = () => {
             Add
           </button>
         </div>
-        <table class="table table-responsive table-striped table-bordered">
-          <thead class="thead-dark">
+        <table className="table table-responsive table-striped table-bordered">
+          <thead className="thead-dark">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">Category</th>
+              <th scope="col">Image1</th>
+              <th scope="col">Image2</th>
+              <th scope="col">Image3</th>
+              <th scope="col">Image4</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>
-                <FontAwesomeIcon
-                  icon={faEdit}
-                  className="home_section_edit"
-                  onClick={() => openUpdateModal()}
-                />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className="home_section_delete"
-                  onClick={() => openDeleteModal()}
-                />
-              </td>
-            </tr>
+            {data.homesection1_id !== undefined ? (
+              <>
+                <tr>
+                  <td>{data.homesection1_title}</td>
+                  <td>{data.homesection1_description}</td>
+                  <td>{data.homesection1_category}</td>
+                  <td>
+                    <img
+                      src={AppUrl.image_url + data.homesection1_image1}
+                      alt={data.homesection1_title + " image1"}
+                    />
+                  </td>
+                  <td>
+                    <img
+                      src={AppUrl.image_url + data.homesection1_image2}
+                      alt={data.homesection1_title + " image2"}
+                    />
+                  </td>
+                  <td>
+                    <img
+                      src={AppUrl.image_url + data.homesection1_image3}
+                      alt={data.homesection1_title + " image3"}
+                    />
+                  </td>
+                  <td>
+                    <img
+                      src={AppUrl.image_url + data.homesection1_image4}
+                      alt={data.homesection1_title + " image4"}
+                    />
+                  </td>
+
+                  <td>
+                    <FontAwesomeIcon
+                      icon={faEdit}
+                      className="home_section_edit"
+                      onClick={() => openUpdateModal(data.homesection1_id)}
+                    />
+                    <FontAwesomeIcon
+                      icon={faTimes}
+                      className="home_section_delete"
+                      onClick={() => openDeleteModal(data.homesection1_id)}
+                    />
+                  </td>
+                </tr>
+              </>
+            ) : (
+              <></>
+            )}
           </tbody>
         </table>
       </div>
@@ -142,13 +309,19 @@ const HomeSection1 = () => {
             <FontAwesomeIcon
               icon={faTimes}
               className="home_section_delete"
-              onClick={() => closeModal()}
+              onClick={() => closeModal(data.homesection1_id)}
             />
           </div>
           <div className="card-body">
             <label>Title:</label>
             <div className="form-group">
-              <input type="text" placeholder="Title" className="form-control" />
+              <input
+                type="text"
+                placeholder="Title"
+                className="form-control"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </div>
 
             <label>Description:</label>
@@ -157,31 +330,64 @@ const HomeSection1 = () => {
                 type="text"
                 placeholder="Description"
                 className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <label>Category:</label>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Category"
+                className="form-control"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
               />
             </div>
 
             <label>Image1:</label>
             <div className="form-group">
-              <input type="file" className="form-control" />
+              <input
+                type="file"
+                className="form-control"
+                onChange={(e) => setImage1(e.target.files[0])}
+              />
             </div>
 
             <label>Image2:</label>
             <div className="form-group">
-              <input type="file" className="form-control" />
+              <input
+                type="file"
+                className="form-control"
+                onChange={(e) => setImage2(e.target.files[0])}
+              />
             </div>
 
             <label>Image3:</label>
             <div className="form-group">
-              <input type="file" className="form-control" />
+              <input
+                type="file"
+                className="form-control"
+                onChange={(e) => setImage3(e.target.files[0])}
+              />
             </div>
 
             <label>Image4:</label>
             <div className="form-group">
-              <input type="file" className="form-control" />
+              <input
+                type="file"
+                className="form-control"
+                onChange={(e) => setImage4(e.target.files[0])}
+              />
             </div>
 
             <div className="form-group">
-              <button className="btn btn-primary form-control" type="button">
+              <button
+                className="btn btn-primary form-control"
+                type="button"
+                onClick={() => addData()}
+              >
                 Add
               </button>
             </div>
@@ -192,7 +398,7 @@ const HomeSection1 = () => {
       {/* update data modal */}
       <div
         className="home_section_modal_update inactive_home_section_modal_update"
-        id="home_section_modal_update"
+        id={"home_section_modal_update" + data.homesection1_id}
       >
         <div className="card">
           <div className="card-header">
@@ -200,13 +406,19 @@ const HomeSection1 = () => {
             <FontAwesomeIcon
               icon={faTimes}
               className="home_section_delete"
-              onClick={() => closeUpdateModal()}
+              onClick={() => closeUpdateModal(data.homesection1_id)}
             />
           </div>
           <div className="card-body">
             <label>Title:</label>
             <div className="form-group">
-              <input type="text" placeholder="Title" className="form-control" />
+              <input
+                type="text"
+                placeholder="Title"
+                className="form-control"
+                value={title_up}
+                onChange={(e) => setTitleUp(e.target.value)}
+              />
             </div>
 
             <label>Description:</label>
@@ -215,11 +427,28 @@ const HomeSection1 = () => {
                 type="text"
                 placeholder="Description"
                 className="form-control"
+                value={description_up}
+                onChange={(e) => setDescriptionUp(e.target.value)}
+              />
+            </div>
+
+            <label>Category:</label>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Category"
+                className="form-control"
+                value={category_up}
+                onChange={(e) => setCategoryUp(e.target.value)}
               />
             </div>
 
             <div className="form-group">
-              <button className="btn btn-success form-control" type="button">
+              <button
+                className="btn btn-success form-control"
+                type="button"
+                onClick={() => updateData(data.homesection1_id)}
+              >
                 Update
               </button>
             </div>
@@ -230,7 +459,7 @@ const HomeSection1 = () => {
       {/* delete data modal */}
       <div
         className="home_section_modal_delete inactive_home_section_modal_delete"
-        id="home_section_modal_delete"
+        id={"home_section_modal_delete" + data.homesection1_id}
       >
         <div className="card">
           <div className="card-header">
@@ -238,14 +467,18 @@ const HomeSection1 = () => {
             <FontAwesomeIcon
               icon={faTimes}
               className="home_section_delete"
-              onClick={() => closeDeleteModal()}
+              onClick={() => closeDeleteModal(data.homesection1_id)}
             />
           </div>
           <div className="card-body">
             <label>Do you want to delete it?</label>
 
             <div className="form-group">
-              <button className="btn btn-danger form-control" type="button">
+              <button
+                className="btn btn-danger form-control"
+                type="button"
+                onClick={() => deleteData(data.homesection1_id)}
+              >
                 Delete
               </button>
             </div>
@@ -257,7 +490,7 @@ const HomeSection1 = () => {
       <div
         className="modal_blur_bg inactive_modal_blur_bg"
         id="modal_blur_bg"
-        onClick={() => closeModal()}
+        onClick={() => closeModal(data.homesection1_id)}
       ></div>
     </>
   );
